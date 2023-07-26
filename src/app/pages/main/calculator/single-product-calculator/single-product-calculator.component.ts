c765b36af9af0f3a9a5dec783d4778615d9be4eb
godhanidenis <godhanidenis@gmail.com>
2023-07-26 18:07:08 +0530

    Change single product calcu.

--- src/app/pages/main/calculator/single-product-calculator/single-product-calculator.component.ts
+++ src/app/pages/main/calculator/single-product-calculator/single-product-calculator.component.ts
@@ -12,6 +12,29 @@ export class SingleProductCalculatorComponent implements OnInit {
   amazonCommission: number = 15;
   orderProcessingFee: number = 6;
   returnProcessingFee: number = 2;
+
+  productDetails = {
+    mpn: '10-02S-160B',
+    currentBBPrice: 115.72,
+    currentBBHolder: 'Seller1',
+    upc: '849657027110',
+    last7DaysLowest: 113.72,
+    maxBBHolder7Days: 'Seller2',
+    productName: '1" Two Tone Preen Collection Curtain Rod 160-240 i',
+    last30DaysLowestBBPrice: 112.72,
+    maxBBHolder30Days: 'Seller3',
+    asin: 'B07H622PBC',
+  };
+
+  calculatorList: any[] = [
+    { name: 'Unit Price', per: 60, price: 60, upDown: 0 },
+    { name: 'Shipping Cost', per: 15, price: 15, upDown: '' },
+    { name: 'Amazon Commission', per: 20, price: 20, upDown: '' },
+    { name: 'Order Processing Fee', per: 2, price: 2, upDown: '' },
+    { name: 'Return Processing Fee', per: 6, price: 6, upDown: '' },
+    { name: 'Retailer Price', per: 100, price: 100, upDown: 0 },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
