fa265066c317659ef83615145287c459b1a6c9e1
godhanidenis <godhanidenis@gmail.com>
2023-07-27 15:08:17 +0530

    Create calculator design.

--- src/app/pages/main/calculator/calculator.component.ts
+++ src/app/pages/main/calculator/calculator.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.scss'],
 })
 export class CalculatorComponent implements OnInit {
+  show = false;
+  description: string =
+    'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam consequatur neque dolor earum, dolorem iste maxime nam distinctio omnis repudiandae tempore similique aliquid animi exercitationem obcaecati ullam corrupti dolorum expedita.';
+
   constructor() {}
 
   ngOnInit(): void {}
