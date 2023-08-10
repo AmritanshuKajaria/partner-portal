435a1f97a16495baf70b12099f562ac8b1247462
godhanidenis <godhanidenis@gmail.com>
2023-08-10 19:02:07 +0530

    Plan UI changes

--- src/app/pages/main/new-calculator/new-calculator.component.ts
+++ src/app/pages/main/new-calculator/new-calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { planDataObj } from 'src/app/shared/constants/constants';
 
 @Component({
   selector: 'app-new-calculator',
@@ -6,11 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-calculator.component.scss'],
 })
 export class NewCalculatorComponent {
+  showCalculator = false;
   show = false;
   description: string =
     'Manage your Amazon Landed Retail Price with our new tool. Break down your price into five components: Unit Price, Amazon Selling Fees, Shipping, Order Processing Fees, and Return Costs. Set, view and adjust these elements yourself to achieve your desired retail price.';
 
-  constructor() {}
+  constructor() {
+    this.showCalculator =
+      planDataObj.plan.current_plan !== 'basic' ||
+      planDataObj.plan.currently_on_trial;
+  }
 
   ngOnInit(): void {}
 }
--- src/app/pages/main/plans/plans.component.ts
+++ src/app/pages/main/plans/plans.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { planDataObj } from 'src/app/shared/constants/constants';
 
 @Component({
   selector: 'app-plans',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plans.component.scss'],
 })
 export class PlansComponent implements OnInit {
+  planDataObj = planDataObj;
   constructor() {}
   ngOnInit(): void {}
 }
