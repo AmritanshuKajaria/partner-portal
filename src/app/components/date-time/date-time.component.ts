841ed70500709d5c08fdb3242aa79cef2d4903f1
jay7587 <jay.godhani2018@gmail.com>
2023-11-24 19:54:05 +0530

    Small Changes

--- src/app/components/date-time/date-time.component.ts
+++ src/app/components/date-time/date-time.component.ts
@@ -7,26 +7,7 @@ import * as moment from 'moment-timezone';
   templateUrl: './date-time.component.html',
   styleUrls: ['./date-time.component.scss'],
 })
-export class DateTimeComponent implements OnInit {
+export class DateTimeComponent {
   @Input() dateTime = '';
-
-  time: string | null = '';
-
-  timeFormate = 'hh:MM a';
-
-  constructor(private datePipe: DatePipe) {}
-
-  ngOnInit(): void {
-    if (this.dateTime) {
-      const formattedTime = this.datePipe.transform(
-        this.dateTime,
-        this.timeFormate
-      );
-
-      const userTimeZoneAbbreviation = moment.tz.guess(true);
-      const abbreviation = moment.tz(userTimeZoneAbbreviation).format('z');
-
-      this.time = `${formattedTime} (${abbreviation})`;
-    }
-  }
+  @Input() timeZone = '';
 }
