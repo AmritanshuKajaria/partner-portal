0cf0701feae499698b3c3889d156ea7fa0956787
godhanidenis <godhanidenis@gmail.com>
2024-08-28 10:30:16 +0530

    Add input field class.

---
+++ src/app/pages/main/setting/setting-component/order-processing-return/order-processing-return.component.ts
@@ -0,0 +1,195 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { EnabledCarriersOptions } from 'src/app/shared/constants/constants';
+import { CommonService } from 'src/app/shared/service/common.service';
+import { FormValidationService } from 'src/app/shared/service/form-validation.service';
+
+@Component({
+  selector: 'app-order-processing-return',
+  templateUrl: './order-processing-return.component.html',
+  styleUrls: ['./order-processing-return.component.scss'],
+})
+export class OrderProcessingReturnComponent implements OnInit {
+  isLoading: boolean = false;
+  inventoryFeedForm!: FormGroup;
+  dropDownList: any = null;
+  enabledCarriersOptions: { name: string; value: string }[] =
+    EnabledCarriersOptions;
+  formFieldOnUI = {
+    inventoryFeedType: true,
+    inventoryFeedDetailType: true,
+    inventoryBucket: true,
+    inventorySchedule: true,
+    inventoryFeedMPN: true,
+    inventoryFeedQuantityColumnName: true,
+    authorizedFeedSenders: true,
+  };
+
+  constructor(
+    private commonService: CommonService,
+    private fb: FormBuilder,
+    private formValidationService: FormValidationService
+  ) {}
+
+  ngOnInit(): void {
+    this.commonService.getJsonData().subscribe(
+      (res) => {
+        this.dropDownList = res;
+        this.formControl['inventoryFeedType'].setValue(1);
+        this.formControl['inventoryFeedDetailType'].setValue(1);
+        this.formControl['inventoryBucket'].setValue(2);
+      },
+      (error) => {
+        console.error('Error fetching JSON data', error);
+      }
+    );
+
+    this.inventoryFeedForm = this.fb.group({
+      inventoryFeedType: [{ value: '', disabled: true }],
+      inventoryFeedDetailType: [
+        {
+          value: '',
+          disabled: true,
+        },
+      ],
+      inventoryBucket: [{ value: '', disabled: true }],
+      inventorySchedule: [[], [Validators.required]],
+      inventoryFeedMPN: ['', [Validators.required]],
+      inventoryFeedQuantityColumnName: ['', [Validators.required]],
+      authorizedFeedSenders: this.fb.array([]),
+    });
+    this.addAuthorizedFeedSender();
+
+    this.inventoryFeedForm?.valueChanges.subscribe((selectedValues) => {
+      this.onFormChange();
+    });
+  }
+
+  get formControl() {
+    return this.inventoryFeedForm.controls;
+  }
+
+  get authorizedFeedSenders(): FormArray {
+    return this.inventoryFeedForm.controls[
+      'authorizedFeedSenders'
+    ] as FormArray;
+  }
+
+  onFormChange(): void {
+    if (
+      this.formControl['inventoryFeedType'].value === 1 &&
+      this.formControl['inventoryFeedDetailType'].value === 1
+    ) {
+      this.formFieldOnUI['inventoryFeedQuantityColumnName'] = true;
+    } else {
+      this.formFieldOnUI['inventoryFeedQuantityColumnName'] = false;
+    }
+
+    if (this.formControl['inventoryFeedType'].value === 1) {
+      this.formFieldOnUI['authorizedFeedSenders'] = true;
+      this.formFieldOnUI['inventoryFeedMPN'] = true;
+    } else {
+      this.formFieldOnUI['authorizedFeedSenders'] = false;
+      this.formFieldOnUI['inventoryFeedMPN'] = false;
+    }
+  }
+
+  newAuthorizedFeedSender() {
+    return this.fb.group({
+      email: ['', [Validators.required]],
+    });
+  }
+
+  addAuthorizedFeedSender() {
+    this.authorizedFeedSenders.push(this.newAuthorizedFeedSender());
+  }
+
+  removeAuthorizedFeedSender(i: number) {
+    this.authorizedFeedSenders.removeAt(i);
+  }
+
+  reset() {
+    for (let index = 0; index < this.authorizedFeedSenders.length; index++) {
+      this.authorizedFeedSenders.removeAt(0);
+    }
+
+    this.inventoryFeedForm?.reset();
+    this.formControl['inventoryFeedType'].setValue(1);
+    this.formControl['inventoryFeedDetailType'].setValue(1);
+    this.formControl['inventoryBucket'].setValue(2);
+    if (this.authorizedFeedSenders.length === 0) {
+      this.addAuthorizedFeedSender();
+    }
+  }
+
+  submitForm() {
+    const valid = this.formValidationService.checkFormValidity(
+      this.inventoryFeedForm,
+      this.formFieldOnUI
+    );
+    if (valid) {
+      this.isLoading = true;
+      const payload = {
+        inventoryFeedType: this.formFieldOnUI['inventoryFeedType']
+          ? this.formControl['inventoryFeedType'].value
+          : '',
+        inventoryFeedDetailType: this.formFieldOnUI['inventoryFeedDetailType']
+          ? this.formControl['inventoryFeedDetailType'].value
+          : '',
+        inventoryBucket: this.formFieldOnUI['inventoryBucket']
+          ? this.formControl['inventoryBucket'].value
+          : '',
+        inventorySchedule: this.formFieldOnUI['inventorySchedule']
+          ? this.formControl['inventorySchedule'].value
+          : '',
+        inventoryFeedMPN: this.formFieldOnUI['inventoryFeedMPN']
+          ? this.formControl['inventoryFeedMPN'].value
+          : '',
+        inventoryFeedQuantityColumnName: this.formFieldOnUI[
+          'inventoryFeedQuantityColumnName'
+        ]
+          ? this.formControl['inventoryFeedQuantityColumnName'].value
+          : '',
+        authorizedFeedSenders: this.formFieldOnUI['authorizedFeedSenders']
+          ? this.formControl['authorizedFeedSenders'].value?.map(
+              (item: any) => item?.email
+            )
+          : '',
+      };
+      setTimeout(() => {
+        console.log(payload);
+        this.isLoading = false;
+      }, 500);
+    } else {
+      Object.values(this.inventoryFeedForm.controls).forEach((control) => {
+        if (control.invalid) {
+          if (control instanceof FormControl) {
+            control.markAsDirty();
+            control.updateValueAndValidity({ onlySelf: true });
+          }
+
+          if (control instanceof FormArray) {
+            control.controls.forEach((formGroup: any) => {
+              Object.values(formGroup.controls).forEach((arrayControl: any) => {
+                if (arrayControl.invalid) {
+                  arrayControl.markAsDirty();
+                  arrayControl.updateValueAndValidity({ onlySelf: true });
+                }
+              });
+            });
+          }
+        }
+      });
+    }
+  }
+
+  goBack() {
+    window.history.back();
+  }
+}
--- src/app/pages/main/setting/setting-routing.module.ts
+++ src/app/pages/main/setting/setting-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SettingComponent } from './setting.component';
 import { MapHandlingComponent } from './setting-component/map-handling/map-handling.component';
 import { InventoryFeedComponent } from './setting-component/inventory-feed/inventory-feed.component';
+import { OrderProcessingReturnComponent } from './setting-component/order-processing-return/order-processing-return.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'inventory-feed',
     component: InventoryFeedComponent,
+  },
+  {
+    path: 'order-processing-return',
+    component: OrderProcessingReturnComponent,
   },
 ];
 
