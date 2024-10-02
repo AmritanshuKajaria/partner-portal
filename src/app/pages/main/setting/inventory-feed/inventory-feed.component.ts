9c8746642a8420507f6ad20d4d8a4b0a86ac7695
godhanidenis <godhanidenis@gmail.com>
2024-10-02 10:43:21 +0530

    inventroty-feed mok api

--- src/app/pages/main/setting/inventory-feed/inventory-feed.component.ts
+++ src/app/pages/main/setting/inventory-feed/inventory-feed.component.ts
@@ -7,8 +7,11 @@ import {
   Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { forkJoin } from 'rxjs';
 import { CommonService } from 'src/app/shared/service/common.service';
 import { FormValidationService } from 'src/app/shared/service/form-validation.service';
+import { PartnerService } from 'src/app/shared/service/partner.service';
 
 @Component({
   selector: 'app-inventory-feed',
@@ -17,8 +20,12 @@ import { FormValidationService } from 'src/app/shared/service/form-validation.se
 })
 export class InventoryFeedComponent implements OnInit {
   isLoading: boolean = false;
+  isSaving: boolean = false;
   inventoryFeedForm!: FormGroup;
   dropDownList: any = null;
+  inventoryFeedData: any;
+
+  // Update this to hide or show inputs on the UI
   formFieldOnUI = {
     inventoryFeedType: true,
     inventoryFeedDetailType: true,
@@ -33,25 +40,15 @@ export class InventoryFeedComponent implements OnInit {
     private commonService: CommonService,
     private fb: FormBuilder,
     private formValidationService: FormValidationService,
-    private router: Router
+    private router: Router,
+    private partnerService: PartnerService,
+    private message: NzMessageService
   ) {}
 
   ngOnInit(): void {
     this.isLoading = true;
-    this.commonService.getJsonData().subscribe(
-      (res) => {
-        this.dropDownList = res;
-        this.formControl['inventoryFeedType'].setValue(1);
-        this.formControl['inventoryFeedDetailType'].setValue(1);
-        this.formControl['inventoryBucket'].setValue(2);
-        this.isLoading = false;
-      },
-      (error) => {
-        console.error('Error fetching JSON data', error);
-        this.isLoading = false;
-      }
-    );
 
+    // Initialize form
     this.inventoryFeedForm = this.fb.group({
       inventoryFeedType: [{ value: '', disabled: true }],
       inventoryFeedDetailType: [
@@ -82,11 +79,32 @@ export class InventoryFeedComponent implements OnInit {
     });
     this.addAuthorizedFeedSender();
 
+    // Set value change listner on form
     this.inventoryFeedForm?.valueChanges.subscribe((selectedValues) => {
       this.onFormChange();
     });
+
+    // API calls
+    forkJoin([
+      this.commonService.getJsonData(),
+      this.partnerService.getPartner(),
+    ]).subscribe({
+      next: ([jsonData, partnerData]: any) => {
+        console.log(partnerData.payload);
+
+        this.inventoryFeedData = partnerData.payload.inventoryDetails;
+        this.patchFormValue(this.inventoryFeedData);
+        this.dropDownList = jsonData;
+        this.isLoading = false;
+      },
+      error: (e) => {
+        this.message.create('error', 'Something went wrong fetching the data');
+        this.isLoading = false;
+      },
+    });
   }
 
+  // Get Form Control
   get formControl() {
     return this.inventoryFeedForm.controls;
   }
@@ -97,6 +115,7 @@ export class InventoryFeedComponent implements OnInit {
     ] as FormArray;
   }
 
+  // Handle hide show inputs
   onFormChange(): void {
     if (
       this.formControl['inventoryFeedType'].value === 1 &&
@@ -116,41 +135,69 @@ export class InventoryFeedComponent implements OnInit {
     }
   }
 
+  // Create new authorized feed sender
   newAuthorizedFeedSender() {
     return this.fb.group({
       email: ['', [Validators.required, Validators.email]],
     });
   }
 
+  // Add authorized feed sender
   addAuthorizedFeedSender() {
     this.authorizedFeedSenders.push(this.newAuthorizedFeedSender());
   }
 
+  // Remove authorized feed sender
   removeAuthorizedFeedSender(i: number) {
     this.authorizedFeedSenders.removeAt(i);
   }
 
+  // Reset form
   reset() {
     for (let index = 0; index < this.authorizedFeedSenders.length; index++) {
       this.authorizedFeedSenders.removeAt(0);
     }
-
     this.inventoryFeedForm?.reset();
-    this.formControl['inventoryFeedType'].setValue(1);
-    this.formControl['inventoryFeedDetailType'].setValue(1);
-    this.formControl['inventoryBucket'].setValue(2);
     if (this.authorizedFeedSenders.length === 0) {
       this.addAuthorizedFeedSender();
     }
+    this.patchFormValue(this.inventoryFeedData);
   }
 
+  // Patch value to the form
+  patchFormValue(data: any) {
+    this.formControl['inventoryFeedType'].setValue(
+      Number(data?.inventoryFeedType)
+    ); 
+    this.formControl['inventoryFeedDetailType'].setValue(
+      Number(data?.inventoryFeedDetailType)
+    );
+    this.formControl['inventoryBucket'].setValue(Number(data?.inventoryBucket));
+    this.formControl['inventorySchedule'].setValue(
+      data?.inventoryFeedFrequency?.map((value: any) => Number(value)) || []
+    );
+    this.formControl['inventoryFeedMPN'].setValue(data?.inventoryFeedMPN);
+    this.formControl['inventoryFeedQuantityColumnName'].setValue(
+      data?.inventoryFeedQuantityColumnName
+    );
+
+    this.authorizedFeedSenders.clear();
+    data.authorizedFeedSenders.forEach((email: any) => {
+      const emailFormGroup = new FormGroup({
+        email: new FormControl(email), // Create a FormControl for email
+      });
+      this.authorizedFeedSenders.push(emailFormGroup); // Push FormGroup into FormArray
+    });
+  }
+
+  // Submit form
   submitForm() {
     const valid = this.formValidationService.checkFormValidity(
       this.inventoryFeedForm,
       this.formFieldOnUI
     );
     if (valid) {
-      this.isLoading = true;
+      this.isSaving = true;
       const payload = {
         inventoryFeedType: this.formFieldOnUI['inventoryFeedType']
           ? this.formControl['inventoryFeedType'].value
@@ -179,8 +226,38 @@ export class InventoryFeedComponent implements OnInit {
           : '',
       };
       setTimeout(() => {
-        console.log(payload);
-        this.isLoading = false;
+        console.log('payload::', payload);
+
+        this.partnerService.updatePartner(payload).subscribe({
+          next: (res) => {
+            this.message.create('success', 'Data Updated Successfully!');
+            this.isSaving = false;
+
+            // Fetch the updated partner data after a successful update
+            this.isLoading = true;
+            this.partnerService.getPartner().subscribe({
+              next: (res: any) => {
+                this.patchFormValue(res.payload.inventoryDetails);
+                this.isLoading = false;
+              },
+              error: (error) => {
+                this.message.create(
+                  'error',
+                  error?.error_message?.[0] ||
+                    'Something went wrong fetching the data'
+                );
+                this.isLoading = false;
+              },
+            });
+          },
+          error: (error: any) => {
+            this.message.create(
+              'error',
+              error?.error_message?.[0] || 'Data Update failed!'
+            );
+            this.isSaving = false; // Ensure saving state is updated on error
+          },
+        });
       }, 500);
     } else {
       Object.values(this.inventoryFeedForm.controls).forEach((control) => {
@@ -205,6 +282,7 @@ export class InventoryFeedComponent implements OnInit {
     }
   }
 
+  // Navigate back
   goBack() {
     this.router.navigate(['/main/setting']);
   }
