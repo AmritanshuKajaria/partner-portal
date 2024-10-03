dee2effc6a332f26ef4bb76de330b8ebf6709fe5
godhanidenis <godhanidenis@gmail.com>
2024-10-03 14:04:34 +0530

    remove code

--- src/app/shared/service/partner.service.ts
+++ src/app/shared/service/partner.service.ts
@@ -18,21 +18,6 @@ export class PartnerService {
     return this.http.post(this.url + '/update-partner', payload);
   }
 
-  // getPartnerPdf(data: any) {
-  //   const promise = new Promise((resolve, reject) => {
-  //     setTimeout(() => {
-  //       resolve(
-  //         {
-  //           "success": true,
-  //           "fileId": "270981474",
-  //           "processed_at": "2024-10-03T04:06:43.000Z",
-  //           "url": "https://api.123stores.com:8080/files/5112ae8814d159271cd57129363c4883.pdf",
-  //           "expired_at": "2024-10-03T04:11:43.000Z"
-  //       });
-  //     }, 1000);
-  //   });
-  //   return from(promise);
-  // }
 
   getPartnerPdf(fileId: string) {
     
